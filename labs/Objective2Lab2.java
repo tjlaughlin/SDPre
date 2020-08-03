ed075b7450083b47a423f6122b607b70d60b8bb8
Tj Laughlin <tlaughl1@gmail.com>
2020-08-03 17:01:30 -0400

    complete objective2lab2

---
+++ labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Timothy Laughlin";
+    int age = 28;
+    String state = "North Carolina";
+    System.out.println("Name:" + name + " age:" + age + " State:" + state);
+  }
+}
